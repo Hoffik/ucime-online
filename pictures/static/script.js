5720291e7ead0648a6aa0793c8231bad2d99bd93
Petr Hofman <hofman.petr@post.cz>
2020-03-27 23:18:56 +0100

    Added appearing text to pictures

---
+++ pictures/static/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+document.getElementById("picture1").addEventListener("click", function(){
+    document.getElementById("text1").style.visibility = "visible";
+});
+document.getElementById("picture2").addEventListener("click", function(){
+    document.getElementById("text2").style.visibility = "visible";
+});
+document.getElementById("picture3").addEventListener("click", function(){
+    document.getElementById("text3").style.visibility = "visible";
+});
+document.getElementById("picture4").addEventListener("click", function(){
+    document.getElementById("text4").style.visibility = "visible";
+});
